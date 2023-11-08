b9fc0eb8bf0a2d0fdd7a2dc284817130a2bc1390
Rajat Mehra <rajat.m.5278@gmail.com>
2023-11-08 17:54:25 +0530

    update backend host url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://socialhq-api.up.railway.app";
+export const API_URL = "https://breakable-erin-long-underwear.cyclic.app";
 export const EMAIL = "testuser@xyz.com";
 export const PASSWORD = "123456";
